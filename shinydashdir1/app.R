55e01c6b9ab31a057d2f39e0e245ef7f3c7fc466
Zoë Oladokun <Zoeoladokun@gmail.com>
2025-09-24 23:55:16 +0100

    Add summary value boxes to dashboard
    Introduces mean, median, and standard deviation value boxes to the dashboard UI. Enhances layout with columns and updates box styling for improved visual organization.

--- shinydashdir1/app.R
+++ shinydashdir1/app.R
@@ -16,17 +16,31 @@ ui <- dashboardPage(
   dashboardBody(
     
     fluidRow(
+      column(width = 6,
       box(
         title = "Select a Number",
+        background = "black",
         status="warning",
+        width = NULL,
+        height = 312,
         sliderInput(inputId = "number",
                     label = "",
-                    value = 500, min = 25, max = 1000)),
+                    value = 500, min = 25, max = 1000))),
+      
       
       box(
         title = "Histogram",
+        solidHeader=TRUE,
+        background ="light-blue",
         status="primary",
+        width = NULL,
         plotOutput("hist", height = 250))
+    ),
+    
+    column(width = 4,
+    valueBoxOutput("meanBox", width = NULL),
+    valueBoxOutput("medianBox", width = NULL),
+    valueBoxOutput("sdBox", width = NULL)
     )
     
   )
@@ -36,12 +50,33 @@ ui <- dashboardPage(
 server <- function(input, output) {
   
   histdata <- reactive({runif(input$number,min=0,max=1)})
+  
   output$hist <- renderPlot({
     
     hist(histdata(),xlab="Value",
          main=paste(input$number,"random values between 0 and 1"))
   })
-     
+  
+  output$meanBox <- renderValueBox({
+    valueBox(
+      round(mean(histdata()),3),"Mean",
+      color = "navy"
+    )
+  })
+  
+  output$medianBox <- renderValueBox({
+    valueBox(
+      round(median(histdata()),3),"Median",
+      color = "aqua"
+    )
+  })
+  
+  output$sdBox <- renderValueBox({
+    valueBox(
+      round(sd(histdata()),3), "Standard Deviation",
+      color = "blue"
+    )
+  })
 }
 
 # Run the application 
